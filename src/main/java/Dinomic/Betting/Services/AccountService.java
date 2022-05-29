5aeb80f7b1143d08284a43e01c0a32134be83a17
Dinomic <66655868+Dinomic@users.noreply.github.com>
2022-05-29 22:03:08 +0700

    use yaml > properties | adjust account service

--- src/main/java/Dinomic/Betting/Services/AccountService.java
+++ src/main/java/Dinomic/Betting/Services/AccountService.java
@@ -2,10 +2,12 @@ package Dinomic.Betting.Services;
 
 import Dinomic.Betting.DAOs.Account;
 import Dinomic.Betting.Security.IAccountRepository;
+import Dinomic.Betting.Services.Interfaces.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-public class AccountService {
+@Service
+public class AccountService implements IAccountService {
 
     @Autowired
     private IAccountRepository accountRepository;
--- src/main/java/Dinomic/Betting/Services/Interfaces/IAccountService.java
+++ src/main/java/Dinomic/Betting/Services/Interfaces/IAccountService.java
@@ -3,7 +3,6 @@ package Dinomic.Betting.Services.Interfaces;
 import Dinomic.Betting.DAOs.Account;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface IAccountService {
     Account getAccountByName(String name);
 }
